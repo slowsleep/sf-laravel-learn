20ec17658d31be8fb56f636d7a76031c77dc7ab6
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-09-28 16:58:35 +0300

    Edit routes. Add home: route, controller, view. Add post resource (route, resource controller)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('home', [HomeController::class, 'index']);
+
+Route::get('about', function () {
+    dd("about");
+});
+
+Route::resource('post', PostController::class);
