1b911b98b5018a9e9b4114301e48c553e360fbcb
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-09-29 19:27:17 +0300

    Add layout view

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Module59 test</title>
+</head>
+<body>
+    <header>
+        <p align="center">header</p>
+        <a href="/">главная</a>
+        <a href="/home">домой</a>
+    </header>
+    @yield('content')
+    <footer>
+        <p align="center">footer</p>
+    </footer>
+</body>
+</html>
