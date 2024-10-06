2829ac6decbe6568e12085f16a2f174a8aea2f27
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-10-06 14:17:43 +0300

    Add migration to add column to posts table

---
+++ database/migrations/2024_10_06_110537_add_subtitle_to_posts.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            if (!Schema::hasColumn('posts', 'subtitle')) {
+                $table->string('subtitle')->nullable()->after('title');
+
+                $table->index(['subtitle']);
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            if (Schema::hasColumn('posts', 'subtitle')) {
+                $table->dropIndex(['subtitle']);
+                $table->dropColumn('subtitle');
+            }
+        });
+    }
+};
