6e3779f4b90c85656a06258df1c46978d8fcf5cc
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-10-07 22:01:47 +0300

    Add PostFactory

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $userIds = User::pluck('id')->toArray();
+
+        return [
+            'title' => fake()->sentence(),
+            'subtitle' => fake()->sentence(),
+            'content' => fake()->text(),
+            'author' => $this->faker->randomElement($userIds),
+        ];
+    }
+}
