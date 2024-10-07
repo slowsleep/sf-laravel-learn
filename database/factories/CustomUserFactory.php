e9fb5a7efeb7edb97666e5067af6963835a56dfe
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-10-07 22:01:32 +0300

    Add CustomUserFactory

---
+++ database/factories/CustomUserFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class CustomUserFactory extends Factory
+{
+    protected $model = User::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => 'admin',
+            'email' => 'admin@admin.admin',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => null,
+        ];
+    }
+}
