9d849048ab86c5eb27a0e25a40a5188bd6c32921
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-10-08 18:50:23 +0300

    Add relationship method tasks to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,4 +49,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Post::class);
     }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
