763297cbe81bdbf2cf135f833a95a3f1164c6285
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-10-11 19:33:54 +0300

    Add console command to find cube of number

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('cube {num}', function ($num) {
+    $this->info($num ** 3);
+});
