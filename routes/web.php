c5f448946c8e6d49d121dc8b02ee5c2af68e1574
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-10-16 18:36:41 +0300

    Add laravel breeze (auth). Edit classes of components and layouts (guest, primary-button)

--- routes/web.php
+++ routes/web.php
@@ -1,39 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\MarkController;
-use App\Http\Controllers\MathController;
-use App\Http\Controllers\TaskController;
-use App\Services\MathService;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('home', [HomeController::class, 'index']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('about', function () {
-    dd("about");
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::resource('post', PostController::class);
-
-Route::get('mark', [MarkController::class, 'index'])->name('mark');
-
-Route::resource('task', TaskController::class)->only(['index', 'store', 'edit', 'update', 'destroy'])->middleware('dblog');
-
-Route::get('math', [MathController::class, 'index']);
-
-Route::get('math/add/{a}/{b}', function (MathService $math, $a, $b) {
-    dd($math->add($a, $b));
-});
-
-Route::get('math/mult/{a}/{b}', function (MathService $math, $a, $b) {
-    dd($math->mult($a, $b));
-});
-
-Route::get('math/pow/{a}/{b}', function (MathService $math, $a, $b) {
-    dd($math->pow($a, $b));
-});
+require __DIR__.'/auth.php';
